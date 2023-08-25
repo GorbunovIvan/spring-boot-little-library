bf016d63ef1f02104e6d56f08069de119685f070
GorbunovIvan <gorbunovvivan@gmail.com>
2023-08-25 08:59:12 +0300

    init

---
+++ src/main/java/com/example/SpringBootLittleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLittleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLittleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootlittlelibrary/SpringBootLittleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlittlelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLittleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
